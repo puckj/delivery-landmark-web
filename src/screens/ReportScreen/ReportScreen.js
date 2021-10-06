2fa8c8337546fbe6862cc74f720b31d5b3f90f33
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2021-10-06 15:40:27 +0700

    add getLandmarkDetail service

--- src/screens/ReportScreen/ReportScreen.js
+++ src/screens/ReportScreen/ReportScreen.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import {
+    getLandmarkDetail
+} from '../../services/services.js'
 
 export default function ReportScreen({
     history
 }) {
+    useEffect(() => {
+        const firstLoad = async () => {
+            await getLandmarkDetail(null)
+                .then(res => {
+                    console.log(res, 'TAG01');
+                })
+                .catch(err => {
+                    console.error(err, 'TAG_ERR');
+                })
+        }
+        firstLoad()
+    }, [])
     return (
         <NavBar history={history} >
             <p>report test</p>
